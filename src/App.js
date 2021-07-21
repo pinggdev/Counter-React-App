61c2643ed25adae1a4e19ec93cb5a3fb9ee48857
KevinSinagaa <kevinsinaga11.ks@gmail.com>
2021-07-22 00:16:40 +0700

    create todos state and todos divs, create todo elements

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import { useState } from "react";
+import classnames from "classnames";
 
 import "./App.css";
 
 import shoppingIcon from "./assets/shopping-icon.svg";
+import plusIcon from "./assets/plus-icon.svg";
+import minusIcon from "./assets/minus-icon.svg";
 
 function App() {
   const [value, setValue] = useState("");
-
-  console.log(value);
+  const [todos, setTodos] = useState([
+    { title: "Susu ultra", count: 1 },
+    { title: "Tahu sumedang", count: 1 },
+    { title: "Semangka", count: 1 },
+  ]);
 
   return (
     <>
@@ -31,6 +37,37 @@ function App() {
             add
           </button>
         </form>
+
+        {todos.length > 0 ? (
+          <div className="todos">
+            {todos.map((todo, index, arr) => {
+              return (
+                <div
+                  key={index}
+                  className={`todo ${
+                    !(arr.length === index + 1) && "todo-divider"
+                  }`}
+                >
+                  {todo.title}
+
+                  <div className="todo-icon-wrapper">
+                    <div className="todo-count">{todo.count}</div>
+
+                    <button className="todo-action-button">
+                      <img src={minusIcon} alt="minus icon" />
+                    </button>
+
+                    <button className="todo-action-button">
+                      <img src={plusIcon} alt="plus icon" />
+                    </button>
+                  </div>
+                </div>
+              );
+            })}
+          </div>
+        ) : (
+          <div>Kosong</div>
+        )}
       </section>
     </>
   );
